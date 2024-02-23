894d59a029cf91a4a910568fd92bf7316142c1e6
Burke Landon <128099556+Burke-LJX@users.noreply.github.com>
2024-02-23 09:47:45 -0500

    Update Tile.java

--- Tile.java
+++ Tile.java
@@ -1,8 +1,32 @@
-import Ship.ShipType;
-
 public class Tile {
-    boolean IsOccupied;
-    boolean IsShot;
-    Ship.ShipType ShipName;
+    private boolean isOccupied;
+    private boolean isShot;
+    private Ship.ShipType shipType;
 
+    public Tile() {
+        this.isOccupied = false;
+        this.isShot = false;
+        this.shipType = null;
+    }
+
+    public void occupyTile(Ship.ShipType shipType) {
+        this.isOccupied = true;
+        this.shipType = shipType;
+    }
+
+    public void shootTile() {
+        this.isShot = true;
+    }
+
+    public boolean isOccupied() {
+        return isOccupied;
+    }
+
+    public boolean isShot() {
+        return isShot;
+    }
+
+    public Ship.ShipType getShipType() {
+        return shipType;
+    }
 }
