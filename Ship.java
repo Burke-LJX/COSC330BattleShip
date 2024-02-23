3703f81e89319136526764fbfe00417c200170ad
Burke Landon <128099556+Burke-LJX@users.noreply.github.com>
2024-02-23 09:46:07 -0500

    Update Ship.java
    Fixed the enum declaration, as well as made some basic functions

--- Ship.java
+++ Ship.java
@@ -1,18 +1,45 @@
 public class Ship {
-    boolean IsSunk;
-    enum ShipType {
-        Destroyer,
-        Submarine,
-        Cruiser,
-        Battleship,
-        Carrier
+    private boolean isSunk;
+    private ShipType shipType;
+    private int size;
+
+    public Ship(ShipType shipType) {
+        this.shipType = shipType;
+        this.size = shipType.getSize();
+        this.isSunk = false;
     }
+
     public void setIsSunk(boolean isSunk) {
-        IsSunk = isSunk;
+        this.isSunk = isSunk;
     }
+
     public boolean getIsSunk() {
-        return IsSunk;
+        return isSunk;
+    }
+
+    public ShipType getShipType() {
+        return shipType;
+    }
+
+    public int getSize() {
+        return size;
     }
 }
 
+public enum ShipType {
+    DESTROYER(2),
+    SUBMARINE(3),
+    CRUISER(3),
+    BATTLESHIP(4),
+    CARRIER(5);
 
+    private final int size;
+
+    ShipType(int size) {
+        this.size = size;
+    }
+
+    public int getSize() {
+        return size;
+    }
+}
