b452079da42a341121676a97c6f1d2e80d1042c5
EBreckenridge <123997908+EBreckenridge@users.noreply.github.com>
2024-02-29 02:54:57 -0500

    Add files via upload
    More work done on MainScreen. Possible alterations to other .java files.

--- MainScreen.java
+++ MainScreen.java
@@ -2,10 +2,9 @@ import java.awt.*;
 import java.awt.event.*;
 
 import javax.swing.*;
-import javax.swing.BoxLayout;
 import javax.swing.border.Border;
-import Tile;
-import Grid;
+
+
 //import Drag.java;
 
         /*NOTES:
@@ -15,35 +14,113 @@ import Grid;
             click grid, check if hit/miss, update imgIcon, disable that specific button
         */
 public class MainScreen extends MouseAdapter{
-        //Declare and initialize a panel for main screen
+        //Delcare and initialize panel to hold mainScreen
+        JPanel mainScreen = new JPanel();
+
+        //Declare and initialize a panel for player elements
         JPanel playerScreen = new JPanel(new BorderLayout());
+        //Declare and initialize a panel for player elements
+        JPanel enemyScreen = new JPanel(new BorderLayout());
         //Declare and initialize JPanels for the grid and ship register
         JPanel gridPanel = new JPanel(new GridLayout());
         JPanel shipPanel = new JPanel(new GridBagLayout());
         private final int GRIDSIZE = 10;
-        JButton[][] playerBoard = new JButton[GRIDSIZE][GRIDSIZE];
+        JButton[][] eBoard = new JButton[GRIDSIZE][GRIDSIZE];
     
-        MainScreen() {
+        MainScreen(SelfGrid playerGrid, EnemyGrid enemyGrid) {
+            mainScreen.setLayout(new BoxLayout(mainScreen, BoxLayout.PAGE_AXIS));
+            mainScreen.setBackground(Color.DARK_GRAY);
+            //Set up vertical spacing for game screen
+            mainScreen.add(Box.createRigidArea(new Dimension(0,5)));
+            mainScreen.add(enemyScreen);
+            mainScreen.add(Box.createRigidArea(new Dimension(0, 10)));
+            mainScreen.add(playerScreen);
+            mainScreen.add(Box.createRigidArea(new Dimension(0,5)));
+
+
+
+            //Creating playerScreen
+
+            //Creating/Displaying SelfGrid
+            JPanel pGrid = new JPanel(new GridLayout());
+            JButton[][] pBoard = new JButton[GRIDSIZE][GRIDSIZE];
+            for(int row = 0; row < GRIDSIZE; row++) {
+                for (int column = 0; column < GRIDSIZE; column++) {
+                    Tile tempTile = playerGrid.getTile(row, column);
+                    JButton buttonTile = tempTile.getTileButton();
+                    pBoard[row][column] = buttonTile;
+                    buttonTile.setBackground(Color.BLUE);
+
+                    pGrid.add(pBoard[row][column]);
+                }
+            }   //REMEMBER: to set pGrid to inactive after ship placement
+
+            //Drag and Drop Listener Assignment to playerScreen
+            ClickListener clickListener = new ClickListener();
+            playerScreen.addMouseListener(clickListener);
+            DragListener dragListener = new DragListener();
+            playerScreen.addMouseMotionListener(dragListener);
+
+
+            //Create shipPanel in playerScreen
             Border shipPanelBorder = createShipPanelBorder();
             shipPanel.setBorder(shipPanelBorder);
-            shipPanel.setBackground(Color.DARK_GRAY);
+            shipPanel.setBackground(Color.GRAY);
+            //NEED TO: Add ship images to shipPanel, add click listeners? to ship image icons?
 
+
+            //Adds pGrid and shipPanel to playerScreen
+            playerScreen.add(pGrid, SwingConstants.LEFT);
+            playerScreen.add(shipPanel, SwingConstants.RIGHT);
+
+
+
+            //Create enemyScreen
+
+            //Mouse click listeners on gridPanel to effect both enemy grid panel and fire button
+            JPanel enemyDisplay = new JPanel(new GridBagLayout());
+
+            //Creating/Displaying enemyGrid
             for(int row = 0; row < GRIDSIZE; row++) {
                 for(int column = 0; column < GRIDSIZE; column++) {
-                    JButton buttonTile = new JButton();
-                    playerBoard[row][column] = buttonTile;
+                    Tile tempTile = enemyGrid.getTile(row, column);
+                    JButton buttonTile = tempTile.getTileButton();
+                    eBoard[row][column] = buttonTile;
                     buttonTile.addMouseListener(this);
-
 
                     gridPanel.add(buttonTile);
                 }
             }
-            //Set up mouse listeners and Drag.java implementation on playerscreen
-            //Mouse click listeners on gridPanel and Drag.java to effect both grid panel and ship panel
-            playerScreen.add(gridPanel, SwingConstants.LEFT);
-            playerScreen.add(shipPanel, SwingConstants.RIGHT);
-        }
-            
+
+            /*Create enemyDisplay in enemyScreen (enemy ship list and fire button)
+            */
+            JButton fireButton = new JButton();
+            GridBagConstraints eDisplayConstraints = new GridBagConstraints();
+
+            //Adds gridPanel and enemyDisplay to enemyScreen
+            enemyScreen.add(gridPanel, SwingConstants.LEFT);
+            enemyScreen.add(enemyDisplay, SwingConstants.RIGHT);
+
+        }   //End of MainScreen constructor
+        
+    private class ClickListener extends MouseAdapter{
+	    public void mousePressed(MouseEvent event) {
+			prevPoint = event.getPoint();
+		}	
+	}
+    private class DragListener extends MouseMotionAdapter{
+    	public void mouseDragged(MouseEvent event) {
+    		Point currPoint = event.getPoint();
+    		int dx = (int) (currPoint.getX() - prevPoint.getX());
+    		int dy = (int) (currPoint.getY() - prevPoint.getY());
+    		
+    		imageUpperLeft.translate(dx, dy);
+    		prevPoint = currPoint;
+    		repaint();  		
+    	}
+	}
+
+
         //Function that creates border for shipPanel
         Border createShipPanelBorder() {
             Border raisedbevel = BorderFactory.createRaisedBevelBorder();
@@ -56,5 +133,12 @@ public class MainScreen extends MouseAdapter{
             //What happens when mouse is clicked
         }
 
-}
 
+        public static void main(String[] args) {
+            // Create players and grids
+            SelfGrid player1Grid = new SelfGrid();
+            EnemyGrid player2Grid = new EnemyGrid();
+            
+            MainScreen game = new MainScreen(player1Grid, player2Grid);
+        }
+}
